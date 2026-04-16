0d4a95ba98bab080707ea89cba08bec0ff5ebe04
samalavishnu <174127750+samalavishnu@users.noreply.github.com>
2026-04-16 11:17:44 +0530

    updated frontend urls

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://vercel.com/018vishnuteja-7545s-projects/mentor-mentee-frontend/6mwWWJEAHsxpgdpPgFcdsf9uu3Ti',
         changeOrigin: true,
       }
     }
